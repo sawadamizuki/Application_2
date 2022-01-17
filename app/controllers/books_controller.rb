c395f64b54dc21b71d6bd7b3da0a0cd3df1976bf
EC2 Default User <ec2-user@ip-172-31-37-137.ec2.internal>
2022-01-17 12:13:09 +0000

    create機能の作成まで、ログインの確認

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,17 @@
 class BooksController < ApplicationController
+
+　def new
+　  @book = Book.new
+　end
+
+　def create
+　  book = Book.new(book_params)
+　  book.save
+　  redirect_to book_path
+
+
+　private
+　def book_params
+　  params.require(:book) .permit(:title, :body)
+　end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+
+  def index
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def show
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+  private
+  def user_params
+  　params.require(:user) .permit(:name, :introduction, :profile_image)
+  end
+
 end
